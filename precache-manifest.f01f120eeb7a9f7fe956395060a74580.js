f38a65f481949a992da4b0276e9f519328f86fe9
Donnie Irvandy <don@donnies-imac.lan>
2020-07-05 17:49:10 -0400

    Updates

---
+++ precache-manifest.f01f120eeb7a9f7fe956395060a74580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50300555858d091f894ee08e9c5d7f2",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "7063f266c4919d261a92",
+    "url": "/portfolio-create-react-app/static/css/main.f505fa27.chunk.css"
+  },
+  {
+    "revision": "57c011ee7d982c12156b",
+    "url": "/portfolio-create-react-app/static/js/2.d4e89820.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio-create-react-app/static/js/2.d4e89820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7063f266c4919d261a92",
+    "url": "/portfolio-create-react-app/static/js/main.3c0e6116.chunk.js"
+  },
+  {
+    "revision": "f2b8295b299fac858656",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.08eb8b6c.js"
+  }
+]);
